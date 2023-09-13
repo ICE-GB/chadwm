f58a0d12b3e9b0ed600adddbeec12b48ef8fec11
awnlzw <awnlzw@126.com>
2023-09-14 00:06:46 +0800

    shell

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-export DWM=$HOME/.config/chadwm/
+export DWM=$HOME/.config/chadwm
 xrdb merge ~/.config/chadwm/.Xresources
-xbacklight -set 10 &
+xbacklight -set 100 &
 feh --bg-fill ~/Pictures/wallpaper/*.* &
 xset r rate 200 50 &
 ~/.dwm/.bin/start_picom.sh &
 
 ~/.config/chadwm/scripts/bar.sh &
-while type chadwm >/dev/null; do chadwm && continue || break; done
+while type ~/.config/chadwm/chadwm/dwm >/dev/null; do ~/.config/chadwm/chadwm/dwm && continue || break; done
